1279e4393cbce445b383fcbd8f77a643f07d1b96
Anders <anders_thy@rocketmail.com>
2020-05-15 11:10:11 +0200

    init

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+var state = { db: null }
+
+exports.connect = function (done) {
+	if (state.db) return done()
+
+	MongoClient.connect('mongodb+srv://group:group123@devenv-swzwi.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
+		.then((client) => {
+			state.db = client.db('notes')
+			done()
+		})
+		.catch((err) => console.log(err))
+}
+
+exports.get = function () {
+	return state.db
+}
