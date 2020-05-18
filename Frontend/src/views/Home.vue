6f0b7d324fc1c6886f200d3f0789c22d8551c391
Izabella Madsen <1067462@ucn.dk>
2020-05-18 14:00:55 +0200

    created header

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div class="columns">
+      <div class="column">
+        this is your home page
+      </div>
+      <div class="column">
+          this is another column
+      </div>
+    </div>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-    name: 'Home',
-    components: {
-        HelloWorld,
-    },
-}
+// export default {
+//     name: 'Home',
+//     components: {
+//         HelloWorld,
+//     },
+// }
 </script>
