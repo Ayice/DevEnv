6ed440b8de70e1182bcec20c59b28c7a126959bc
Izabella Madsen <1067462@ucn.dk>
2020-05-19 12:40:34 +0200

    Added footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <Header />
-    <router-view />
+    <div id="content-wrap">
+		<router-view />
+	</div>
+	<Footer />
   </div>
 </template>
 <script>
 	import Header from './components/Header'
+		import Footer from './components/Footer'
+
 	export default {
 		components: {
 			Header,
+			Footer
 		},
 	}
 </script>
 <style lang="scss">
+#content-wrap {
+	padding-bottom: 2.5rem;
+}
 </style>
