483fdc5c87824145586df5b33ad1df52b77afc94
Izabella Madsen <1067462@ucn.dk>
2020-05-19 11:38:17 +0200

    created emner files

---
+++ Frontend/src/components/Emner.vue
@@ -0,0 +1,34 @@
+<template>
+  <a :href="kategori.id" class="kategori">{{kategori.title}}</a>
+</template>
+
+<script>
+	export default {
+		name: 'Hovedkategori',
+		props: {
+			kategori: {
+				title: {
+					type: String,
+					required: true,
+				},
+				id: {
+					type: String,
+					required: true,
+				},
+			},
+		},
+	}
+</script>
+
+<style lang="scss">
+	.kategori {
+		font-weight: 600;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100px;
+		background-color: rgba(113, 184, 188, 0.452);
+		color: #ffffff;
+		width: 100%;
+	}
+</style>
---
+++ Frontend/src/views/EmnerContainer.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column has-text-centered">
+        <h1 class="title is-2">Hovedkategorier</h1>
+        <h2
+          class="subtitle is-6"
+        >Tryk på en hovedkategori og tilvælg emner du ønsker at få nyheder om</h2>
+      </div>
+    </div>
+
+    <section class="hovedkategori__container columns is-mobile is-multiline is-1">
+      <div class="column is-6" v-for="kategori in Liste" :key="kategori.id">
+        <Hovedkategori :kategori="kategori" />
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+	import Hovedkategori from '../components/Hovedkategori'
+	export default {
+		name: 'HovedkategoriContainer',
+		components: {
+			Hovedkategori,
+		},
+		data() {
+			return {
+				Liste: [
+					{
+						title: 'kategori 1',
+						id: '1235dfsa',
+					},
+					{
+						title: 'kategori 2',
+						id: '1235dfsa21231',
+					},
+					{
+						title: 'kategori 3',
+						id: '1235f123423r',
+					},
+				],
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	.hovedkategori__container {
+		width: 90%;
+		background-color: #355a93;
+		margin: auto;
+	}
+</style>
