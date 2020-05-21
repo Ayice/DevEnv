688d5bc5c7ce8c140408e836ed904b50d2dbe516
Anders <anders_thy@rocketmail.com>
2020-05-21 11:08:51 +0200

    Removed example test

--- Frontend/tests/unit/Button.spec.js
+++ Frontend/tests/unit/Button.spec.js
@@ -2,17 +2,6 @@ import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import Button from '@/components/login/Button.vue'
 
-// import HelloWorld from '@/components/HelloWorld.vue'
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg },
-//     })
-//     expect(wrapper.text()).to.include(msg)
-//   })
-// })
-
 describe('Button', () => {
   it('Should render props.btntext when passed', () => {
     const btntext = 'click me'
