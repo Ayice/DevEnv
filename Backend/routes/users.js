a8e3ef3306784056fd1480b65e064d871e3b0122
Anders <anders_thy@rocketmail.com>
2020-05-25 15:37:20 +0200

    formatting

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,28 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../db');
-var mongodb = require('mongodb');
+var express = require('express')
+var router = express.Router()
+var db = require('../db')
+var mongodb = require('mongodb')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	var collection = db.get().collection('users');
+router.get('/', function (req, res, next) {
+	var collection = db.get().collection('users')
 
-	collection.find().toArray().then(result => {
-        res.send(result);
-        console.log(result);
-	});
-});
+	collection
+		.find()
+		.toArray()
+		.then(result => {
+			res.send(result)
+			console.log(result)
+		})
+})
+
 /* POST user to db */
-router.post('/', function(req, res, next) {
-	var collection = db.get().collection('users');
+router.post('/', function (req, res, next) {
+	var collection = db.get().collection('users')
 
-	collection.insertOne({ name: req.body.name}, (err, result) => {
-		if (err) throw err;
-		res.send('User saved to db');
-		console.log('User saved to db');
+	collection.insertOne({ name: req.body.name }, (err, result) => {
+		if (err) throw err
+		res.send('User saved to db')
+		console.log('User saved to db')
 	})
+})
 
-});
-
-
-module.exports = router;
+module.exports = router
