890e552ac53653c7bf0ee1734b0ac0bc8716345f
Anders <anders_thy@rocketmail.com>
2020-06-07 15:20:14 +0200

    Use bulma variables and mixins
    Created a file that imports all the utilities from Bulma, so we can use them in the different components

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import '../node_modules/bulma/bulma.sass'
+import 'bulma'
 
 Vue.config.productionTip = false
 
 new Vue({
-	router,
-	store,
-	render: h => h(App),
+  router,
+  store,
+  render: h => h(App),
 }).$mount('#app')
